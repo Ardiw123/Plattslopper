aa8dc66f4e9d0bdbc01c9f6c6b554ea5302167a3
Ardiw123 <arvid.wetterlind@gmail.com>
2026-09-21 14:19:33 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+using SFML.Audio;
+
+namespace platformer;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		using (var window = new RenderWindow(new VideoMode(new Vector2u(800, 600)), "Plattsopper"))
+		{
+			window.Closed += (o, e) => window.Close();
+			// TODO: Initialize
+			Clock clock = new Clock();
+			while (window.IsOpen)
+			{
+				window.DispatchEvents();
+				float deltaTime = clock.Restart().AsSeconds();
+				// TODO: Updates
+				window.Clear();
+				// TODO: Drawing
+				window.Display();
+			}
+		}
+	}
+}
